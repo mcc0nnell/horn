fd557255518d9e9846eb6cf6492d2b1c05ffe373
Robert McConnell <robert@mcc0nnell.org>
2026-09-07 15:41:46 -0400

    Register claim atomicity as Celix service

---
+++ runtime/celix/bundles/handbook_2003_claim_atomicity/Activator.cc
@@ -0,0 +1,26 @@
+#include "horn/rules/IHornRule.h"
+
+#include "celix/BundleActivator.h"
+
+#include <memory>
+
+namespace horn::rules {
+std::shared_ptr<IHornRule> createClaimAtomicityRule();
+}
+
+class ClaimAtomicityBundleActivator final {
+public:
+    explicit ClaimAtomicityBundleActivator(const std::shared_ptr<celix::BundleContext>& ctx) {
+        auto rule = horn::rules::createClaimAtomicityRule();
+        const auto metadata = rule->metadata();
+        registration = ctx->registerService<horn::rules::IHornRule>(std::move(rule))
+                           .addProperty("horn.rule.id", metadata.ruleId)
+                           .addProperty("horn.rule.profile", metadata.profile)
+                           .build();
+    }
+
+private:
+    std::shared_ptr<celix::ServiceRegistration> registration{};
+};
+
+CELIX_GEN_CXX_BUNDLE_ACTIVATOR(ClaimAtomicityBundleActivator)
