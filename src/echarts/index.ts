d1cefc69dc50a27ccd8b4c87fff2389cc0786fcb
Robert McConnell <robert@mcc0nnell.org>
2026-09-07 15:28:58 -0400

    Export ECharts analytical surface

---
+++ src/echarts/index.ts
@@ -0,0 +1,15 @@
+export {
+  projectArgument,
+  projectEvidence,
+  projectHornDocument,
+  projectTimeline,
+  type HornEChartsProjection,
+  type HornEChartsView,
+} from "./projections";
+
+export {
+  mountHornECharts,
+  mountHornEChartsTriptych,
+  type HornEChartsController,
+  type HornEChartsTriptych,
+} from "./mount";
